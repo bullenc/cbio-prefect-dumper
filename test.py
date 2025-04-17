c85dd0e04bc2fc726e116a227dc5eac5bf0c4287
akmukherjee <akmukherjee@gmail.com>
2025-04-17 17:31:59 -0400

    deleting unused

--- test.py
+++ test.py
@@ -11,18 +11,11 @@ def create_dump(
     password,
     database,
     port=3306,
-    dump_family_only=True,
     output_dir="./dumps",
 ):
     os.makedirs(output_dir, exist_ok=True)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-
-    if dump_family_only:
-        dump_file = os.path.join(output_dir, f"{database}_family_{timestamp}.sql")
-        tables = "family"
-    else:
-        dump_file = os.path.join(output_dir, f"{database}_full_{timestamp}.sql")
-        tables = ""
+    dump_file = os.path.join(output_dir, f"{database}_dump_{timestamp}.sql")
 
     command = [
         "mysqldump",
@@ -59,7 +52,7 @@ def create_dump(
 
 
 @flow(name="rfam-dump-flow-test", log_prints=True)
-def rfam_dump_flow():
+def test_dump_flow():
     DB_CONFIG = {
         "host": "relational.fel.cvut.cz",
         "user": "guest",
@@ -73,11 +66,11 @@ def rfam_dump_flow():
 
 if __name__ == "__main__":
     DB_CONFIG = {
-        "host": "mysql-rfam-public.ebi.ac.uk",
-        "user": "rfamro",
-        "password": "",
-        "database": "Rfam",
-        "port": 4497,
+        "host": "relational.fel.cvut.cz",
+        "user": "guest",
+        "password": "ctu-relational",
+        "database": "Financial_std",
+        "port": 3306,
     }
 
     create_dump(**DB_CONFIG, dump_family_only=True)
