766d67b7cda2b5bd8a0623892380817c4ae50ccd
akmukherjee <akmukherjee@gmail.com>
2025-04-16 22:49:29 -0400

    Updating main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rfam_dumper.dumper import test_db_connection, create_dump
+from prefect import flow
 
 DB_CONFIG = {
     "host": "mysql-rfam-public.ebi.ac.uk",
@@ -9,5 +10,19 @@ DB_CONFIG = {
 }
 
 if __name__ == "__main__":
+    test_db_connection(**DB_CONFIG)
+    create_dump(**DB_CONFIG, dump_family_only=True)
+
+
+@flow(name="rfam-dump-flow")
+def rfam_dump_flow():
+    DB_CONFIG = {
+        "host": "mysql-rfam-public.ebi.ac.uk",
+        "user": "rfamro",
+        "password": "",
+        "database": "Rfam",
+        "port": 4497,
+    }
+
     test_db_connection(**DB_CONFIG)
     create_dump(**DB_CONFIG, dump_family_only=True)
